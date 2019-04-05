ae02b51008f99d160c2b411db4366f80a8b38783
Oleksandr <ssilich@bitbucket.org>
2019-04-05 20:32:09 +0300

    adding tests and test dependency

---
+++ src/main/tests/com/academy/softserve/levytskyi/CountingSortTest.java
@@ -0,0 +1,14 @@
+package com.academy.softserve.levytskyi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CountingSortTest {
+
+    @Test
+    public void start() {
+
+
+    }
+}
