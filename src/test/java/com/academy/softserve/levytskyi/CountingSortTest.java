c12c2be928024f35a1ab4fb969f3e2176dcb32a0
Oleksandr <ssilich@bitbucket.org>
2019-04-08 00:38:07 +0300

    update test in levytskiy

--- src/test/java/com/academy/softserve/levytskyi/CountingSortTest.java
+++ src/test/java/com/academy/softserve/levytskyi/CountingSortTest.java
@@ -1,38 +1,57 @@
 package com.academy.softserve.levytskyi;
 
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 public class CountingSortTest {
+    private static Method sort;
+    private static Method setNumSequence;
 
+    @BeforeClass
+    public static void init() throws NoSuchMethodException {
+        sort = CountingSort.class.getDeclaredMethod("sort", int[].class);
+        sort.setAccessible(true);
 
-    @Test
-    public void testSort() {
-        int[] array = {4, 1, 6, 7, -10, 83, 101, 54};
-        int[] template = {-10, 1, 4, 6, 7, 54, 83, 101};
-
-        CountingSort countingSort = new CountingSort();
-        countingSort.sort(array);
-        assertArrayEquals(array, template);
-        // Пофіксити від'ємні
+        setNumSequence = CountingSort.class.getDeclaredMethod("setNumSequence", String.class);
+        setNumSequence.setAccessible(true);
     }
 
     @Test
-    public void testSetNumSequence() {
+    public void testSort() throws InvocationTargetException, IllegalAccessException {
+        CountingSort countingSort = new CountingSort();
+
+        int[] array = {4, 1, 6, 7, 83, 101, 54};
+        int[] template = {1, 4, 6, 7, 54, 83, 101};
+
+        sort.invoke(countingSort, (Object) array);
+        assertArrayEquals(array, template);
+    }
+
+/*    @Test
+    public void testSetNumSequence() throws InvocationTargetException, IllegalAccessException {
         String arrayInString = "1 8 -3 2 6 8 3";
         int[] template = {1, 8, -3, 2, 6, 8, 3};
         CountingSort countingSort = new CountingSort();
-        int [] array = countingSort.setNumSequence(arrayInString);
+
+
+
+        int [] array = setNumSequence.invoke(countingSort,arrayInString);
 
         assertArrayEquals(array,template);
-    }
 
-    @Test(expected = NumberFormatException.class)
-    public void testSetNumSequenceException() {
+
+        //Зробити, щоб повертало масив
+    }*/
+
+    @Test(expected = Exception.class)
+    public void testSetNumSequenceException() throws InvocationTargetException, IllegalAccessException {
         CountingSort countingSort = new CountingSort();
-        countingSort.setNumSequence("1 5 6 f s df");
+        setNumSequence.invoke(countingSort, "1 5 6 f s df");
     }
 }
--- src/test/java/com/academy/softserve/levytskyi/PathsWithoutCrossingTest.java
+++ src/test/java/com/academy/softserve/levytskyi/PathsWithoutCrossingTest.java
@@ -1,44 +1,63 @@
 package com.academy.softserve.levytskyi;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 import static org.junit.Assert.*;
 
 public class PathsWithoutCrossingTest {
 
+    private static Method calculateCountWays;
+    private static Method countWays;
+
+
+    @BeforeClass
+    public static void init() throws NoSuchMethodException {
+        calculateCountWays = PathsWithoutCrossing.class.getDeclaredMethod("calculateCountWays", int.class);
+        calculateCountWays.setAccessible(true);
+
+        countWays = PathsWithoutCrossing.class.getDeclaredMethod("countWays", int.class);
+        countWays.setAccessible(true);
+    }
 
     @Test
-    public void testCalculateCountWays() {
+    public void testCalculateCountWays() throws InvocationTargetException, IllegalAccessException {
+
+
         PathsWithoutCrossing pathsWithoutCrossing = new PathsWithoutCrossing();
         int template = 16796;
-        int result = pathsWithoutCrossing.calculateCountWays(10);
+        int result = (int) calculateCountWays.invoke(pathsWithoutCrossing, 10);
 
-        assertEquals(template,result);
+        assertEquals(template, result);
     }
 
-    @Test(expected = NegativeArraySizeException.class)
-    public void testCalculateCountWaysNegativeArraySizeException() {
+    @Test(expected = Exception.class)
+    public void testCalculateCountWaysNegativeArraySizeException() throws InvocationTargetException, IllegalAccessException {
         PathsWithoutCrossing pathsWithoutCrossing = new PathsWithoutCrossing();
-        int result = pathsWithoutCrossing.calculateCountWays(-5);
+        int result = (int) calculateCountWays.invoke(pathsWithoutCrossing, -5);
     }
 
-    @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void testCalculateCountWaysArrayIndexOutOfBoundsException() {
+    @Test(expected = Exception.class)
+    public void testCalculateCountWaysArrayIndexOutOfBoundsException() throws InvocationTargetException, IllegalAccessException {
         PathsWithoutCrossing pathsWithoutCrossing = new PathsWithoutCrossing();
-        int result = pathsWithoutCrossing.calculateCountWays(0);
+        int result = (int) calculateCountWays.invoke(pathsWithoutCrossing, 0);
     }
 
 
     @Test
-    public void countWays() {
+    public void countWays() throws InvocationTargetException, IllegalAccessException {
         PathsWithoutCrossing pathsWithoutCrossing = new PathsWithoutCrossing();
 
         int template = 42;
-        int result1 = pathsWithoutCrossing.countWays(10);
-        int result2 = pathsWithoutCrossing.countWays(-10);
+        int result1 = (int) countWays.invoke(pathsWithoutCrossing, 10);
+        int result2 = (int) countWays.invoke(pathsWithoutCrossing, -10);
 
-        assertEquals(template,result1);
-        assertEquals(0,result2);
+        assertEquals(template, result1);
+        assertEquals(0, result2);
 
     }
 }
