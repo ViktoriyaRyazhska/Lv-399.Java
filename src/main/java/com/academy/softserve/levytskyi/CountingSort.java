ad8ad46b1ce7e98afa3e74afa0373fde5fedd2ab
serhiylev <slevitsku@gmail.com>
2019-04-08 13:14:54 +0300

    add return

--- src/main/java/com/academy/softserve/levytskyi/CountingSort.java
+++ src/main/java/com/academy/softserve/levytskyi/CountingSort.java
@@ -49,7 +49,7 @@ public class CountingSort implements Algorithm {
     public void start(final BufferedReader bufferedReader) {
         try {
             System.out.println("To finish tap the Enter\n"
-                    + "Enter the elements dividing them by spaces: ");
+                    + "Enter positive elements dividing them by spaces: ");
             setNumSequence(bufferedReader.readLine());
             sort(numSequence);
             Arrays.stream(numSequence).forEach(value ->
