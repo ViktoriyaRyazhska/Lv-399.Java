40f7053fcecbf7c093ef2e37fad5ab2f1cf5c4ca
Anastasia Hanuliak <anastasia.hanuliak@gmail.com>
2019-04-08 16:46:38 +0300

    split method start(), added method calc() for find out the number of ways of painting the fence

--- src/main/java/com/academy/softserve/nastyastr/PaintFence.java
+++ src/main/java/com/academy/softserve/nastyastr/PaintFence.java
@@ -27,10 +27,31 @@ public class PaintFence implements Algorithm {
     private int n;
 
     /**
+     * @param n number of posts
+     * @param k number of colours
+     * @return count ways
+     */
+    public long calc(int n, int k ){
+        long total = k;
+        int same = 0, diff = k;
+
+        for (int i = 2; i <= n; i++) {
+            same = diff;
+            diff = (int) total * (k - 1);
+            diff = diff % mod;
+            total = (same + diff) % mod;
+        }
+        return total;
+    }
+
+    /**
      * @param bufferedReader
      */
     @Override
     public void start(final BufferedReader bufferedReader) {
+        System.out.println("This is paint fence algorithm:"
+                + "\nFirst - you have to enter number of posts"
+                + " then -  number of colors!");
         System.out.println("Enter numbers of posts: ");
         try {
             n = Integer.parseInt(bufferedReader.readLine());
@@ -41,20 +62,9 @@ public class PaintFence implements Algorithm {
         } catch (IOException io) {
             io.printStackTrace();
         }
-
-        long total = k;
-        int same = 0, diff = k;
-
-        for (int i = 2; i <= n; i++) {
-
-            same = diff;
-            diff = (int) total * (k - 1);
-            diff = diff % mod;
-            total = (same + diff) % mod;
-        }
         System.out.println(n
                 + " posts can be painted in "
-                + k + " colours in " + total + " ways");
+                + k + " colours in " + calc(n,k) + " ways");
     }
 
     /**
@@ -62,8 +72,6 @@ public class PaintFence implements Algorithm {
      */
     @Override
     public String toString() {
-        return "This is paint fence algorithm:"
-                + "\nFirst - you have to enter number of posts"
-                + " then -  number of colors!";
+        return "Paint Fance";
     }
 }
